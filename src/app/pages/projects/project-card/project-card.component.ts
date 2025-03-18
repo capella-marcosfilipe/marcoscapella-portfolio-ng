675cbf34c5492c891edccecb4924fce56e2b4da3
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-18 09:46:49 -0300

    Coded the project-card

--- src/app/pages/projects/project-card/project-card.component.ts
+++ src/app/pages/projects/project-card/project-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   standalone: false,
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.css'
+  styleUrl: './project-card.component.css',
 })
 export class ProjectCardComponent {
-
+  @Input() title: string = 'CODING PROJECT';
+  @Input() link: string = '#';
+  @Input() thumbnail: string = '../../../../assets/images/miscthumb.png';
 }
