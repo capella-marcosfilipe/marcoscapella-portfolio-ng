520777878144f558790e5b2afe559ceb89720e7f
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-18 17:38:49 -0300

    Created the Experience page with the info.

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+import experienceData from '../../../assets/contents/experiences.json';
+
+interface WorkExperience {
+  company: string;
+  role: string;
+  period: string;
+  description: string;
+}
+
+interface AcadExperience {
+  institution: string;
+  course: string;
+  period: string;
+}
+
 @Component({
   selector: 'app-experience',
   standalone: false,
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
-
+  workExperience: WorkExperience[] = experienceData.work_experience;
+  acadExperience: AcadExperience[] = experienceData.academic_experience;
 }
