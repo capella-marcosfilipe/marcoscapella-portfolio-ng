656566ea6df07fe679eb1da8c064e02a6f36b7be
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-04-22 19:16:25 -0300

    Implemented search projects by tech

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import projectsData from '../../../assets/contents/projects.json';
 
 interface Project {
@@ -16,8 +15,30 @@ interface Project {
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
 })
-export class ProjectsComponent {
-  thumbnailsFolder: string = '../../../../assets/images/';
+export class ProjectsComponent implements OnInit {
+  protected projects: Project[] = projectsData.projects;
+  protected filteredProjects: Project[] = [];
 
-  projects: Project[] = projectsData.projects;
+  ngOnInit(): void {
+    this.filteredProjects = this.feedProjects();
+  }
+
+  protected feedProjects(techsFilter: string[] = ['']): Project[] {
+    if (techsFilter.length === 0 || techsFilter[0].trim() === '') {
+      return this.projects;
+    }
+
+    return this.projects.filter((project) =>
+      techsFilter.some((filter) =>
+        project.techs.some((tech) =>
+          tech.toLowerCase().includes(filter.toLowerCase())
+        )
+      )
+    );
+  }
+
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.filteredProjects = this.feedProjects([input.value]);
+  }
 }
